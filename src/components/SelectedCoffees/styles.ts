ed9030c2e1767cc2924afed1cbda6877dd0d2b0d
Alissonv2 <alissonvitorino8@gmail.com>
2023-02-28 13:59:33 -0300

    feat: created coffees selected component

---
+++ src/components/SelectedCoffees/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const SelectedCoffeesContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+	padding: 2rem;
+	gap: 1.5rem;
+
+	min-width: 448px;
+	width: 100%;
+	height: 498px;
+	margin-top: 2rem;
+
+	/* Base/Card */
+
+	background: #f3f2f2;
+	border-radius: 6px 44px;
+`
