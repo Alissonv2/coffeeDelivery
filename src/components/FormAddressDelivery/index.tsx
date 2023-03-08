6109f55fa69f589d806d8fdae10e58c7228d512b
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-08 19:01:55 -0300

    criando componente de formulário

--- src/components/FormAddressDelivery/index.tsx
+++ src/components/FormAddressDelivery/index.tsx
@@ -1,10 +1,100 @@
-import { ContainerInputs, ContainerPayment, FormContainer } from './styles'
+import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import { InputAddress } from '../Inputs/InputFormAddress'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import {
+	ContainerInputs,
+	ContainerPayment,
+	ContainerTitle,
+	FormContainer,
+	FormInputsContainer,
+} from './styles'
+
+const addressFormValidationSchema = zod.object({
+	cep: zod.string().regex(/^[0-9]{5}-?[0-9]{3}$/, 'Informe um cep válido'),
+	rua: zod.string().min(1, 'Informe a rua'),
+	numero: zod.string(),
+	complemento: zod.string(),
+	bairro: zod.string().min(1, 'Informe o bairro'),
+	cidade: zod.string().min(1, 'Informe a cidade'),
+	uf: zod.string().min(1, 'Informe o estado').max(2),
+})
+
+type FormAddressData = zod.infer<typeof addressFormValidationSchema>
 
 export function FormDelivery() {
+	const { register, handleSubmit, formState } = useForm<FormAddressData>({
+		resolver: zodResolver(addressFormValidationSchema),
+		defaultValues: {
+			cep: '63050735',
+			rua: 'Rua das flores',
+			numero: '1632',
+			complemento: '',
+			bairro: 'Romeirão',
+			cidade: 'Juazeiro do norte',
+			uf: 'CE',
+		},
+	})
+
+	function handleFormSubmit(data: FormAddressData) {
+		console.log(data)
+	}
+
 	return (
-		<FormContainer>
-			<ContainerInputs></ContainerInputs>
-			<ContainerPayment></ContainerPayment>
+		<FormContainer onSubmit={handleSubmit(handleFormSubmit)}>
+			<ContainerInputs>
+				<ContainerTitle>
+					<MapPinLine size={25} color={'#c47f17'} />
+					<div>
+						<h1>Endereço de entrega</h1>
+						<p>Informe o endereço onde deseja receber seu pedido</p>
+					</div>
+				</ContainerTitle>
+				<FormInputsContainer>
+					<InputAddress width={30}>
+						<input type="text" placeholder="CEP" {...register('cep')} />
+					</InputAddress>
+					<InputAddress width={100}>
+						<input type="text" placeholder="Rua" {...register('rua')} />
+					</InputAddress>
+					<InputAddress width={30}>
+						<input
+							type="string"
+							placeholder="Número"
+							{...register('numero', { valueAsNumber: true })}
+						/>
+					</InputAddress>
+					<InputAddress width={69} optional>
+						<input
+							type="text"
+							placeholder="Complemento"
+							{...register('complemento')}
+						/>
+					</InputAddress>
+					<InputAddress width={30}>
+						<input type="text" placeholder="Bairro" {...register('bairro')} />
+					</InputAddress>
+					<InputAddress width={60}>
+						<input type="text" placeholder="Cidade" {...register('cidade')} />
+					</InputAddress>
+					<InputAddress width={8.5}>
+						<input type="text" placeholder="UF" {...register('uf')} />
+					</InputAddress>
+				</FormInputsContainer>
+			</ContainerInputs>
+			<ContainerPayment>
+				<ContainerTitle>
+					<CurrencyDollar size={25} color={'#8047F8'} />
+					<div>
+						<h1>Pagamento</h1>
+						<p>
+							O pagamento é feito na entrega. Escolha a forma que deseja pagar
+						</p>
+					</div>
+				</ContainerTitle>
+			</ContainerPayment>
 		</FormContainer>
 	)
 }
--- src/components/FormAddressDelivery/styles.ts
+++ src/components/FormAddressDelivery/styles.ts
@@ -36,3 +36,69 @@ export const ContainerInputs = styled(BaseContainersForm)`
 export const ContainerPayment = styled(BaseContainersForm)`
 	height: 207px;
 `
+
+export const FormInputsContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	width: 100%;
+	gap: 0.2rem;
+`
+
+export const ContainerTitle = styled.div`
+	display: flex;
+	width: 100%;
+	gap: 0.5rem;
+
+	div {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		h1 {
+			/* Text/Regular M */
+
+			font-family: 'Roboto';
+			font-style: normal;
+			font-weight: 400;
+			font-size: 1rem;
+			line-height: 130%;
+			/* identical to box height, or 21px */
+
+			display: flex;
+			align-items: center;
+
+			/* Base/Subtitle */
+
+			color: #403937;
+
+			/* Inside auto layout */
+
+			flex: none;
+			order: 0;
+			align-self: stretch;
+			flex-grow: 0;
+		}
+
+		p {
+			font-family: 'Roboto';
+			font-style: normal;
+			font-weight: 400;
+			font-size: 0.875rem;
+			line-height: 130%;
+			/* or 18px */
+
+			display: flex;
+			align-items: center;
+
+			/* Base/Text */
+
+			color: #574f4d;
+
+			/* Inside auto layout */
+
+			flex: none;
+			order: 1;
+			align-self: stretch;
+			flex-grow: 0;
+		}
+	}
+`
