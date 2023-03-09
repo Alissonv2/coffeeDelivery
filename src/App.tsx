74b6676b02bcd2501659c3e252a2edec7aea0375
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-09 18:31:12 -0300

    inserindo contexto no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
+import { CartContextProvider } from './hooks/ShoppingCart'
 import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
@@ -7,10 +8,12 @@ import { defaultTheme } from './styles/themes/default'
 function App() {
 	return (
 		<ThemeProvider theme={defaultTheme}>
-			<BrowserRouter>
-				<Router />
-				<GlobalStyle />
-			</BrowserRouter>
+			<CartContextProvider>
+				<BrowserRouter>
+					<Router />
+					<GlobalStyle />
+				</BrowserRouter>
+			</CartContextProvider>
 		</ThemeProvider>
 	)
 }
