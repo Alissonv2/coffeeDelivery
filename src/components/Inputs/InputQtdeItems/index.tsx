6d2f7e8752a3fc3d76a8dd71533daf441c086de2
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-09 18:31:48 -0300

    inserindo e removendo item no carrinho

--- src/components/Inputs/InputQtdeItems/index.tsx
+++ src/components/Inputs/InputQtdeItems/index.tsx
@@ -1,16 +1,29 @@
 import { useState } from 'react'
 import { ActionButton, InputContainer } from './styles'
 import { Plus, Minus } from 'phosphor-react'
+import { useCart } from '../../../hooks/ShoppingCart'
 
-export function InputQTDEitems() {
-	const [inputValue, setInputValue] = useState<number>(1)
+type ItemProps = {
+	item: {
+		name: string
+		price: number
+		image: string
+	}
+}
+
+export function InputQTDEitems({ item }: ItemProps) {
+	const { addItem, removeItem } = useCart()
+	const [inputValue, setInputValue] = useState<number>(0)
+	const { name, price, image } = item
 
 	function handleSetInputValue() {
+		addItem({ name, price, image })
 		setInputValue((state) => state + 1)
 	}
 
 	function handlePutInputValue() {
-		if (inputValue > 1) {
+		if (inputValue > 0) {
+			removeItem(name)
 			setInputValue((state) => state - 1)
 		}
 	}
