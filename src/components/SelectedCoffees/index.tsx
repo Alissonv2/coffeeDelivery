9b054d52e886bf7940d2959a946edfc403d3a807
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-10 11:41:53 -0300

    criando listagem de cafés selecionados

--- src/components/SelectedCoffees/index.tsx
+++ src/components/SelectedCoffees/index.tsx
@@ -1,5 +1,61 @@
-import { SelectedCoffeesContainer } from './styles'
+import {
+	ContainerButtons,
+	ContentPriceAndName,
+	ContentProductContainer,
+	ProductCard,
+	ProductName,
+	RemoveProduct,
+	SelectedCoffeesContainer,
+} from './styles'
+import { useCart } from '../../hooks/ShoppingCart'
+import { ProductItem } from '../../@types/products'
+import { calcTotalItemPrice, formatCurrency } from '../../utils/index'
+import { InputQTDEitems } from '../Inputs/InputQtdeItems'
+import { useEffect, useState } from 'react'
 
 export function SelectedCoffees() {
-	return <SelectedCoffeesContainer />
+	const { shoppingCartData, removeProduct } = useCart()
+	const [dataProducts, setDataProducts] = useState<ProductItem[][]>([])
+
+	async function handleProducts(productsArray: ProductItem[]) {
+		const newProductArray = productsArray.reduce((acumullator: any, object) => {
+			const { name } = object
+			if (!acumullator[name]) {
+				acumullator[name] = []
+			}
+			acumullator[name].push(object)
+			return acumullator
+		}, {})
+		const newDataProducts: ProductItem[][] = Object.values(newProductArray)
+		setDataProducts(newDataProducts)
+	}
+
+	useEffect(() => {
+		handleProducts(shoppingCartData?.items)
+	}, [shoppingCartData.items])
+
+	return (
+		<SelectedCoffeesContainer>
+			{dataProducts &&
+				dataProducts.map((item: ProductItem[]) => {
+					return (
+						<ProductCard key={item[0].id}>
+							<img src={item[0].image} alt="" />
+							<ContentProductContainer>
+								<ContentPriceAndName>
+									<ProductName>{item[0].name}</ProductName>
+									<span>{formatCurrency(calcTotalItemPrice(item))}</span>
+								</ContentPriceAndName>
+								<ContainerButtons>
+									<InputQTDEitems item={item[0]} />
+									<RemoveProduct onClick={() => removeProduct(item[0].name)}>
+										Remover
+									</RemoveProduct>
+								</ContainerButtons>
+							</ContentProductContainer>
+						</ProductCard>
+					)
+				})}
+		</SelectedCoffeesContainer>
+	)
 }
--- src/components/SelectedCoffees/styles.ts
+++ src/components/SelectedCoffees/styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
-export const SelectedCoffeesContainer = styled.div`
+export const SelectedCoffeesContainer = styled.section`
 	display: flex;
 	flex-direction: column;
-	align-items: flex-start;
+	align-items: center;
 	padding: 2rem;
 	gap: 1.5rem;
 
@@ -16,4 +16,95 @@ export const SelectedCoffeesContainer = styled.div`
 
 	background: #f3f2f2;
 	border-radius: 6px 44px;
+`
+
+export const ProductCard = styled.div`
+	display: flex;
+	padding: 0.5rem;
+	gap: 0.5rem;
+
+	width: 26rem;
+	height: 5.5rem;
+
+	img {
+		height: 4rem;
+		width: 4rem;
+	}
+`
+
+export const ContentProductContainer = styled.div`
+	display: flex;
+	min-width: 20rem;
+	width: 100%;
+	flex-direction: column;
+	gap: 0.5rem;
+`
+
+export const ContentPriceAndName = styled.header`
+	display: flex;
+	width: 100%;
+	justify-content: space-between;
+	align-items: center;
+
+	span {
+		width: 5rem;
+
+		/* Text/Bold M */
+
+		font-family: 'Roboto';
+		font-style: normal;
+		font-weight: 700;
+		font-size: 1rem;
+		line-height: 130%;
+		/* identical to box height, or 21px */
+
+		display: flex;
+		align-items: center;
+		text-align: right;
+
+		/* Base/Text */
+
+		color: #574f4d;
+	}
+`
+
+export const ProductName = styled.p`
+	width: 20rem;
+
+	font-family: 'Roboto';
+	font-style: normal;
+	font-weight: 400;
+	font-size: 1rem;
+	line-height: 130%;
+	/* identical to box height, or 21px */
+
+	display: flex;
+	align-items: center;
+
+	/* Base/Subtitle */
+
+	color: #403937;
+`
+
+export const RemoveProduct = styled.button`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 72px;
+	height: 38px;
+
+	border: none;
+
+	background: #e6e5e5;
+	border-radius: 6px;
+
+	&:hover {
+		cursor: pointer;
+	}
+`
+
+export const ContainerButtons = styled.div`
+	display: flex;
+	width: 100%;
+	gap: 0.5rem;
 `
