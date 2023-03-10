5cfb90d7ae39b8ea812150e16767825d49fa27d1
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-10 11:42:15 -0300

    criando util para calcular total

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,17 @@
+import { ProductItem } from '../@types/products'
+
 export function formatCurrency(currency: number) {
 	return currency.toLocaleString('pt-br', {
 		style: 'currency',
 		currency: 'BRL',
 	})
+}
+
+export function calcTotalItemPrice(item: ProductItem[]) {
+	const total = item.reduce(
+		(accumulator, currentValue) => accumulator + currentValue.price,
+		0,
+	)
+
+	return total
 }
