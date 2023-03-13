94910349541797dd6eba5a1e9ddac44b7cdb79a3
Alissonv2 <alissonvitorino8@gmail.com>
2023-03-13 18:23:47 -0300

    adicionando lógica para adicionar item caso click direto no carrinho

--- src/components/CardButton/index.tsx
+++ src/components/CardButton/index.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from 'react-router-dom'
 import { ShoppingCart } from 'phosphor-react'
 import { CartButton } from './styles'
-
 interface CardButtonProps {
 	width: number
 	height: number
 	color: string
 	background: string
+	isSetItem?: boolean
+	addItems?: () => void
 }
 
 export function CardButton({
@@ -14,15 +15,26 @@ export function CardButton({
 	height,
 	color,
 	background,
+	isSetItem = false,
+	addItems,
 }: CardButtonProps) {
 	const navigate = useNavigate()
+
+	function handleClickButton() {
+		if (isSetItem && addItems) {
+			addItems()
+			navigate('checkout')
+		} else {
+			navigate('checkout')
+		}
+	}
 	return (
 		<CartButton
 			width={width}
 			height={height}
 			color={color}
 			background={background}
-			onClick={() => navigate('checkout')}
+			onClick={handleClickButton}
 		>
 			<ShoppingCart size={25} weight="fill" />
 		</CartButton>
